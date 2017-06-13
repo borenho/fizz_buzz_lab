ac011a1be3b7ceaa4f49b25eab2d323e1a6c68f9
borenho <borecollins@gmail.com>
2017-06-13 13:57:09 +0300

    Add function to pass all tests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,15 @@
 def fizz_buzz(param):
-    pass
+    """The FizzBuzz Challenge"""
+
+    # Return FizzBuzz if param is divisible by both 3 and 5
+    if param % 3 == 0 and param % 5 == 0:
+        return "FizzBuzz"
+    # Return Fizz if param is divisible by both 3
+    elif param % 3 == 0:
+        return "Fizz"
+    # Return Buzz if param is divisible by 5
+    elif param % 5 == 0:
+        return "Buzz"
+    else:
+    # Return param if param is not divisible by 3 or 5
+        return param
